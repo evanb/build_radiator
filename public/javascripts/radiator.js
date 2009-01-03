cf1d22298dc1f0c53e26998ea4b9fe6f63b3ce38
Evan Bottcher <evan@bottch.com>
2009-01-03 21:41:22 +1100

    sorry can't remember

--- public/javascripts/radiator.js
+++ public/javascripts/radiator.js
@@ -28,13 +28,18 @@ function showUrl(url) {
 function updateBuildStatuses() {
     json = $.getJSON("/build_plans.json", function(results) {
         div = $("<div><ul></ul></div>");
+        combined_status = "green";
         $.each(results, function(i, result) {
             build_plan = result.build_plan;
             li = $("<li>" + build_plan.name + "</li>").addClass("buildStatus").addClass(build_plan.status + "Status");
             div.append(li);
+            if (build_plan.status == "red") {
+                combined_status = "red";
+            }
         });
         $("#build_statuses").children().remove();
         $("#build_statuses").append(div);
+        $("#build_statuses").addClass(combined_status + "BodyStatus");
     });
     window.setTimeout(function() {
         updateBuildStatuses();
