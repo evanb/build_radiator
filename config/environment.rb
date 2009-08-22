e67e2128b2eb2c330dce088c0cdfa9c5b45b5260
Evan Bottcher <evan@evan-bottcher.local>
2009-08-22 22:56:26 +1000

    whoops wrong gem name.

--- config/environment.rb
+++ config/environment.rb
@@ -26,7 +26,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem "rest-client", :version => '1.0.3', :lib => "rest-client"
+  config.gem "rest-client", :version => '1.0.3', :lib => "rest_client"
   
 
   # Only load the plugins named here, in the order given. By default, all plugins 
