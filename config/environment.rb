9cccd416fc604e3ae238c7989b8ee0e1c71f534b
Evan Bottcher <evan@evan-bottcher.local>
2009-08-22 22:43:22 +1000

    frozen rest-client

--- config/environment.rb
+++ config/environment.rb
@@ -25,6 +25,8 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
+
+  config.gem "rest-client", :version => '1.0.3'
   
 
   # Only load the plugins named here, in the order given. By default, all plugins 
